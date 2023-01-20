fee331adb6d35f7de4102940f8ef551960b6647c
caoayu <1401262639@qq.com>
2023-01-20 14:15:22 +0800

    init

---
+++ tools.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Quick-Tools
+// @description  some web tools.
+// @version      0.1
+// @namespace    caoayu
+// @author       caoayu
+// @homepageURL    https://immersive-translate.owenyoung.com/
+// @supportURL    https://github.com/immersive-translate/immersive-translate/
+// @match      *://*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=userscript.net
+// @downloadURL 
+// @updateURL 
+// @inject-into    content
+// @license     AGPL-3.0-or-later
+// @noframes
+// @run-at       document-end
+// @name:zh-CN     网页助手
+// @description:zh-CN     网页自由编辑、密码显示等。
+// ==/UserScript==
+
+(
+    () =>{
+        "use strict";
+        console.log("hello world");
+    }
+)()
